b78fc4c37c35070e4d05ba845d9fb61dba6ce951
carlacou <carlacoutinhopontes@gmail.com>
2022-01-24 00:34:29 -0300

    Refatorando a rota de status

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();
@@ -14,6 +15,7 @@ app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'Sucesso total!' });
 });
 
+app.use(statusRoute);
 // Inicialização do servidor
 app.listen(3000, () => {
   console.log('Aplicação executando na porta 3000!');
